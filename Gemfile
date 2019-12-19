08007c15d1ed7516d77a279bac93e08f01b18b89
Zaire  Swailes <swaileszaire@gmail.com>
2019-12-19 13:01:36 +0000

    Automatically backed up by Learn

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@
 source "https://rubygems.org"
 
 # gem "rails"
+
+def oxford_comma
+
+
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
-def oxford_comma(array)
+def oxford_comma(arr)
+arr = [fiddleheads, okra, kohlrabi]
+when arr(3) each.do |words| {"words, words and words"; end
+when arr(2) each.do |words| {""
+  
 
 end
